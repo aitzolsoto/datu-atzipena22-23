24f60e4469b163d2e2c0a1c144afc6d348a4fbda
Aitzol Soto <aitzoluni@outlook.es>
2022-09-19 13:16:54 +0200

    fitxategiak

---
+++ 1-Fitxategiak/iostreams/src/main/java/dambi/CopyCharactersMaiuskulaz.java
@@ -0,0 +1,32 @@
+package dambi;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class CopyCharactersMaiuskulaz {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("iostreams/xanadu.txt");
+            outputStream = new FileWriter("iostreams/characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                c = Character.toUpperCase(c);
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
